37ee68ce63d379862cee08faf84652623d39ee9c
Naya Singhania <naya.singhania@gmail.com>
2024-10-09 22:21:05 -0700

    add character limit to TextArea

--- src/components/atoms/text-area/component.tsx
+++ src/components/atoms/text-area/component.tsx
@@ -20,6 +20,7 @@ export const TextArea: React.FC<ITextAreaProps> = ({
       {required && <p className="text-red-500">*</p>}
     </div>
     <textarea
+      maxLength={160}
       className={cn(
         "rounded-xl bg-border text-text px-[16px] py-[10px] focus:outline-none w-full placeholder-neutral mb-2 border-border-hovered border-2",
         className,
